2efa198ed5fe1197a2e2d9efcd1e48bd517485af
Iordan Iordanov <iiordanov@gmail.com>
2024-04-20 17:17:16 -0400

    Do not scale resolution up unnecessarily on Macs running the app in Designed for iPad mode.

--- bVNC/app/RemoteSession.swift
+++ bVNC/app/RemoteSession.swift
@@ -364,7 +364,7 @@ class RemoteSession {
         var newScreenWidth = screenWidth
         var newScreenHeight = screenHeight
         
-        if self.stateKeeper.isOnMacOs() {
+        if self.stateKeeper.isOnMacOsOriPadOnMacOs() {
             #if targetEnvironment(macCatalyst)
             globalWindow?.windowScene?.titlebar?.titleVisibility = .hidden
             #endif
